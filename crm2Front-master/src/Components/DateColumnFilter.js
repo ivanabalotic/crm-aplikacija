a5fa9c231510916863bb0e63dfd773463dcc848c
ivanabalotic <ivabalotic@gmail.com>
2022-11-13 22:20:36 +0100

    Revert "Aplikacija skoro gotova"
    This reverts commit f586b76e066683e4ee0bd9d02fb43233b17d7152.

--- crm2Front-master/src/Components/DateColumnFilter.js
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect } from "react";
-import { Form } from "react-bootstrap";
-import { DateTime } from "luxon";
-
-const DateColumnFilter = ({ column }) => {
-  return <div></div>;
-};
-
-export default DateColumnFilter;
