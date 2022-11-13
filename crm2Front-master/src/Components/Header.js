0a049c3c3436a0c3b7b6c56951ecd10a8b01cd03
nikola225 <jovanoviic225@gmail.com>
2022-11-13 22:55:14 +0100

    dodavanje headera

---
+++ crm2Front-master/src/Components/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import "../styles/Header.css";
+
+function Header({ userType = "", setRole, setUserType }) {
+  const history = useHistory();
+
+  const logOut = () => {
+    window.localStorage.removeItem("api_token");
+    window.localStorage.removeItem("role");
+    window.localStorage.removeItem("staff_id");
+    setRole("");
+    setUserType("");
+    history.push("/Login");
+  };
+
+  let logoutButton;
+  if (userType !== "") {
+    logoutButton = (
+      <Button variant="link" onClick={logOut} className="logout-button">
+        Logout
+      </Button>
+    );
+  }
+
+  return (
+    <>
+      <nav className="header">
+        <div className="header-container">
+          <h1 className="header-logo">ITEH CRM 2022</h1>
+          <ul>
+            <li className="header-item-view">{userType}</li>
+            <li className="header-item-logout">{logoutButton}</li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Header;
